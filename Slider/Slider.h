fae7c4579cbfdf925ba2be929afd0db3702c969e
christian otkjær <christian.otkjaer@gmail.com>
2017-01-27 08:39:09 +0100

    Initial Commit

---
+++ Slider/Slider.h
@@ -0,0 +1,19 @@
+//
+//  Slider.h
+//  Slider
+//
+//  Created by Christian Otkjær on 27/01/17.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slider.
+FOUNDATION_EXPORT double SliderVersionNumber;
+
+//! Project version string for Slider.
+FOUNDATION_EXPORT const unsigned char SliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slider/PublicHeader.h>
+
+
